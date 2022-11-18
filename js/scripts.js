8cbb98e5d2d858f9b514425bf22aecf41b3f1b16
dayanjr <dayanfrazao@byui.edu>
2022-11-18 16:01:37 -0700

    up

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,16 @@
 //ADD the key and change units to imperial
-const apiURL = ""
+const apiURL = "//api.openweathermap.org/data/2.5/weather?id=5816861&appid=b85709abfebaacbe0c192a934ab5d901&units=imperial"
 
 //Go fetch it and then wait for a response.
 fetch(apiURL)
   .then((response) => response.json())
-  .then((changeme) => {
+  .then((weatherInfo) => {
     //Once it comes back, display it to the console.
-    console.log(changeme);
+    console.log(weatherInfo);
+    document.getElementById('place').innerHTML=weatherInfo.name;
+    document.getElementById('currentTemp').innerHTML=weatherInfo.main.temp;
+    document.getElementById('windSpeed').innerHTML=weatherInfo.wind.speed;
+    
     
     
 
